61984f8415b169ed690f32f28f31cd0ac4a2bf2f
JoannaLai909 <angellai9499@gmail.com>
2026-04-28 16:06:10 +0800

    Update sdgs_introduction.js

--- sdgs_introduction.js
+++ sdgs_introduction.js
@@ -2,14 +2,13 @@
 window.onscroll = function() {
   let button = document.getElementById("nextBtn");
   console.log("Scroll Position: " + document.documentElement.scrollTop); // 打印滾動位置
-  if (document.documentElement.scrollTop > 1) {
-  if (document.documentElement.scrollTop > 0) {
+  if (document.documentElement.scrollTop > 100) {
     button.style.display = "block"; // 顯示按鈕
   } else {
     button.style.display = "none"; // 隱藏按鈕
   }
 };
-}
+
 document.getElementById("nextBtn").addEventListener("click", function () {
   window.location.href = "purpose.html";
 });
