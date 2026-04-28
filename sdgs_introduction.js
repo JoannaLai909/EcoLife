ccdc14a2e20d93a2e424802f3cc7ec7931a97cbf
JoannaLai909 <angellai9499@gmail.com>
2026-04-28 15:45:48 +0800

    Update sdgs_introduction.js

--- sdgs_introduction.js
+++ sdgs_introduction.js
@@ -1,7 +1,7 @@
 // 當頁面滾動時觸發
 window.onscroll = function() {
   let button = document.getElementById("nextBtn");
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+  if (document.documentElement.scrollTop > 0) {
     button.style.display = "block"; // 顯示按鈕
   } else {
     button.style.display = "none"; // 隱藏按鈕
