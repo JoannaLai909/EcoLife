d43bb82c5c9097f274b52cf9e4de465f2f35f404
JoannaLai909 <angellai9499@gmail.com>
2026-04-28 15:32:34 +0800

    新增按鈕

--- sdgs_introduction.js
+++ sdgs_introduction.js
@@ -1,3 +1,13 @@
+// 當頁面滾動時觸發
+window.onscroll = function() {
+  let button = document.getElementById("nextBtn");
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    button.style.display = "block"; // 顯示按鈕
+  } else {
+    button.style.display = "none"; // 隱藏按鈕
+  }
+};
+
 document.getElementById("nextBtn").addEventListener("click", function () {
   window.location.href = "purpose.html";
 });
