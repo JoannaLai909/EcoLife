0ce215d5ca87cf58ba0fecf93c1f61291beed8c1
JoannaLai909 <angellai9499@gmail.com>
2026-05-05 11:19:33 +0800

    修改

--- entrance.js
+++ entrance.js
@@ -1,3 +1,19 @@
 document.getElementById("startBtn").addEventListener("click", function() {
   window.location.href = "rules.html";
 });
+
+function revealOnScroll() {
+  const elements = document.querySelectorAll('.reveal');
+
+  elements.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add('show');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
