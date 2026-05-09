efc472559965a15b963f2eb451750d4b17fe6b89
JoannaLai909 <angellai9499@gmail.com>
2026-05-09 14:33:05 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,33 +1,4 @@
-const menuBtn = document.getElementById("menuBtn");
 
-const menuPanel = document.getElementById("menuPanel");
-
-menuBtn.addEventListener("click", () => {
-
-    menuPanel.classList.toggle("active");
-
-});
-
-document.getElementById("pauseBtn")
-.addEventListener("click", () => {
-
-    alert("Game paused");
-
-});
-
-document.getElementById("restartBtn")
-.addEventListener("click", () => {
-
-    location.reload();
-
-});
-
-document.getElementById("homeBtn")
-.addEventListener("click", () => {
-
-    window.location.href = "entrance.html";
-
-});
 
 const menuBtn = document.getElementById("menuBtn");
 const menuModal = document.getElementById("menuModal");
