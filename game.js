b476315eb3f529f7a4de69e1f1af051c64c61f1a
JoannaLai909 <angellai9499@gmail.com>
2026-05-09 14:21:37 +0800

    game

--- game.js
+++ game.js
@@ -28,3 +28,23 @@ document.getElementById("homeBtn")
     window.location.href = "entrance.html";
 
 });
+
+const menuBtn = document.getElementById("menuBtn");
+const menuModal = document.getElementById("menuModal");
+const closeMenuBtn = document.getElementById("closeMenuBtn");
+
+menuBtn.addEventListener("click", () => {
+  menuModal.classList.add("active");
+});
+
+closeMenuBtn.addEventListener("click", () => {
+  menuModal.classList.remove("active");
+});
+
+document.getElementById("restartBtn").addEventListener("click", () => {
+  location.reload();
+});
+
+document.getElementById("homeBtn").addEventListener("click", () => {
+  window.location.href = "entrance.html";
+});
