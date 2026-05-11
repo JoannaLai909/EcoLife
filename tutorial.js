8f1f17f74ff88be19d699ca90489e577e2537073
JoannaLai909 <angellai9499@gmail.com>
2026-05-11 22:16:59 +0800

    tutorial

---
+++ tutorial.js
@@ -0,0 +1,31 @@
+const tutorialTexts = [
+    "Welcome to EcoLife Simulator!",
+    "In this game, your daily choices will affect money, energy, and SDG progress.",
+    "Before the game starts, you can choose one SDG category or let the game choose randomly.",
+    "Try to reach the target score before the game ends. Good luck!"
+];
+
+let tutorialStep = 0;
+
+const tutorialText = document.getElementById("tutorialText");
+const nextTutorialBtn = document.getElementById("nextTutorialBtn");
+const tutorialDialog = document.querySelector(".tutorial-dialog");
+
+nextTutorialBtn.addEventListener("click", () => {
+    tutorialStep++;
+
+    if (tutorialStep < tutorialTexts.length) {
+        tutorialText.innerText = tutorialTexts[tutorialStep];
+
+        tutorialDialog.classList.remove("pop");
+        void tutorialDialog.offsetWidth;
+        tutorialDialog.classList.add("pop");
+
+        if (tutorialStep === tutorialTexts.length - 1) {
+            nextTutorialBtn.innerText = "Start";
+        }
+
+    } else {
+        window.location.href = "game.html";
+    }
+});
